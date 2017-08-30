bd3cf343c01fea229c750fc373578dd687d78311
cym <cym@qq.com>
2017-08-29 21:38:18 -0700

    support RX8010 driver

--- create.sh
+++ create.sh
@@ -4,7 +4,7 @@ export ARCH=arm
 
 export CROSS_COMPILE=/usr/local/arm/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-
 
-make mrproper  # means CLEAN
+#make mrproper  # means CLEAN
 
 make imx_v7_defconfig
 
