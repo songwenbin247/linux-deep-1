8fb26a34018c1b85b56f5b9efa5b12d2eace0b02
cym <cym@qq.com>
2018-08-02 21:22:32 -0700

    modift create.sh

--- create.sh
+++ create.sh
@@ -8,12 +8,12 @@ export CROSS_COMPILE=/usr/local/arm/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12
 
 make imx_v7_defconfig
 
-if   [ "$1" = "nand" ]
-then
-	cp arch/arm/boot/dts/imx6ul-14x14-evk_nand.dts arch/arm/boot/dts/imx6ul-14x14-evk.dts
-else
-	cp arch/arm/boot/dts/imx6ul-14x14-evk_emmc.dts arch/arm/boot/dts/imx6ul-14x14-evk.dts
-fi
+#if   [ "$1" = "nand" ]
+#then
+#	cp arch/arm/boot/dts/imx6ul-14x14-evk_nand.dts arch/arm/boot/dts/imx6ul-14x14-evk.dts
+#else
+#	cp arch/arm/boot/dts/imx6ul-14x14-evk_emmc.dts arch/arm/boot/dts/imx6ul-14x14-evk.dts
+#fi
 
 make uImage LOADADDR=0x10008000 -j4
 
